373f9813c01ab4531c46bafb77cacd240d0ef2e3
Hsu Chien <HsuChien@users.noreply.github.com>
2019-04-30 11:19:55 +0800

    Add files via upload

---
+++ key.py
@@ -0,0 +1,12 @@
+﻿import time
+def x_key():
+    x_circle=42
+    x=(time.time()-1556500142.4091797)/60/60/24
+    if x<=x_circle:
+        print(x_circle-x,'欢迎使用')
+        return 1
+    elif x>x_circle:
+        print(x_circle-x,'请联系管理员！')
+        return 0
+
+
